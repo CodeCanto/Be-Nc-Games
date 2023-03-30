d408f51f2a8d2cec11f084afaaa5bc97090ec234
Entelodonto <jamesdanielporter@proton.me>
2023-03-30 10:35:24 +0100

    fixed files

--- db/connection.js
+++ db/connection.js
@@ -3,14 +3,20 @@ const dotenv = require("dotenv");
 
 const ENV = process.env.NODE_ENV || "development";
 
+const config =
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        max: 2,
+      }
+    : {};
+
 dotenv.config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-const PGDATABASE = process.env.PGDATABASE;
-
-if (!PGDATABASE) {
-  throw new Error("PGDATABASE not set");
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
